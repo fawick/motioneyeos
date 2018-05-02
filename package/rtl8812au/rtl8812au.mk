7d147c0df30bf7f0a7217c781bc200bb98d9d49e
Fabian Wickborn <fabian@wickborn.net>
2018-05-02 23:47:05 +0200

    Update rtl8812au for kernel 4.14

--- package/rtl8812au/rtl8812au.mk
+++ package/rtl8812au/rtl8812au.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTL8812AU_VERSION = 3e450d16d9c3e730c67b2a541520e85f664fd101
-RTL8812AU_SITE = $(call github,gnab,rtl8812au,$(RTL8812AU_VERSION))
+RTL8812AU_VERSION = 4d58fd6605ba6247666c4d3a719b552f01a0ffc3
+RTL8812AU_SITE = $(call github,lwfinger,rtl8812au,$(RTL8812AU_VERSION))
 RTL8812AU_LICENSE = GPL-2.0
 RTL8812AU_LICENSE_FILES = LICENSE
 
